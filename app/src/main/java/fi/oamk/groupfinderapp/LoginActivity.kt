ff06c1ee2affe1358fe38465b3c1f07723cc1653
Artem Sitnov <stnoov@gmail.com>
2021-04-07 14:03:47 +0300

    add firebase auth to LoginActivity.kt

--- app/src/main/java/fi/oamk/groupfinderapp/LoginActivity.kt
+++ app/src/main/java/fi/oamk/groupfinderapp/LoginActivity.kt
@@ -3,8 +3,10 @@ package fi.oamk.groupfinderapp
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
@@ -12,18 +14,38 @@ import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity: AppCompatActivity() {
 
+    private val TAG: String = LoginActivity::class.java.name
     private lateinit var auth: FirebaseAuth
-    private lateinit var loginButton: Button
+    private var currentUser: FirebaseUser? = null
+    private lateinit var email: TextView
+    private lateinit var password: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         auth = FirebaseAuth.getInstance()
-        loginButton = findViewById(R.id.loginButton)
     }
 
     fun loginSubmit(view: View) {
-        Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+        email = findViewById(R.id.emailInput)
+        password = findViewById(R.id.passwordInput)
+        login(email.text.toString(), password.text.toString())
+    }
+
+    fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithEmail:success")
+                    currentUser = auth.currentUser
+                    Toast.makeText(baseContext, "Auth success",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(this, "Authentication failed",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
     fun moveToRegister(view: View) {
