1124a4be4264b15fa5866921439cf523f7093938
Artem Sitnov <stnoov@gmail.com>
2021-04-07 13:49:10 +0300

    add functionality to login submit button

--- app/src/main/java/fi/oamk/groupfinderapp/LoginActivity.kt
+++ app/src/main/java/fi/oamk/groupfinderapp/LoginActivity.kt
@@ -1,4 +1,27 @@
 package fi.oamk.groupfinderapp
 
-class LoginActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class LoginActivity: AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var loginButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+        loginButton = findViewById(R.id.loginButton)
+    }
+
+    fun loginSubmit(view: View) {
+        Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+    }
 }
