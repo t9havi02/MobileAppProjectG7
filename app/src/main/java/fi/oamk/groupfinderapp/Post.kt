ebd36ed72fcf2cb830d7712a3e070ae0b8e1235e
Jani Inkala <t9inja00@students.oamk.fi>
2021-04-10 13:54:47 +0300

    post view with no code

---
+++ app/src/main/java/fi/oamk/groupfinderapp/Post.kt
@@ -0,0 +1,11 @@
+package fi.oamk.groupfinderapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Post : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post)
+    }
+}
