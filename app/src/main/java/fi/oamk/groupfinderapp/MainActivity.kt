c01fb56bff5cd1c4bab5e679ccecc08fa024a3fd
Artem Sitnov <stnoov@gmail.com>
2021-04-14 19:27:53 +0300

    add functionality to menu bar

--- app/src/main/java/fi/oamk/groupfinderapp/MainActivity.kt
+++ app/src/main/java/fi/oamk/groupfinderapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package fi.oamk.groupfinderapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_profile -> startActivity(Intent(this, ProfileActivity::class.java))
+            R.id.nav_logout -> {
+                FirebaseAuth.getInstance().signOut()
+                startActivity(Intent(this, LoginActivity::class.java))
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
