5466d88567862c923c7c8dcb0cde873eaf4f7de3
Ville Häyrynen <hayrynenville@outlook.com>
2021-04-19 15:31:58 +0300

    Unfinished list generation SAVE

--- app/src/main/java/fi/oamk/groupfinderapp/MainActivity.kt
+++ app/src/main/java/fi/oamk/groupfinderapp/MainActivity.kt
@@ -3,15 +3,37 @@ package fi.oamk.groupfinderapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        database = Firebase.database.reference
+
+        val posts: ArrayList<Any>
+
+        database.child("posts").get().addOnSuccessListener {
+            val posts:ArrayList<Any> = it.value as ArrayList<Any>;
+
+            posts.forEach {
+                val post = it as HashMap
+            }
+
+        }.addOnFailureListener{
+            Log.e("firebase", "Error getting data", it)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
