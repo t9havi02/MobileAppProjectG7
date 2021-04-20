c9c988a48761b75232a706628026460bb05a2abe
Artem Sitnov <stnoov@gmail.com>
2021-04-20 15:41:20 +0300

    fill text views with actual post info

--- app/src/main/java/fi/oamk/groupfinderapp/PostActivity.kt
+++ app/src/main/java/fi/oamk/groupfinderapp/PostActivity.kt
@@ -1,10 +1,13 @@
 package fi.oamk.groupfinderapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_post.*
 
 class PostActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
@@ -12,6 +15,10 @@ class PostActivity : AppCompatActivity() {
         val data = intent.extras?.getSerializable("POST_KEY") as? Post
 
         if (data != null) {
+            dateView.text = "Date: ${data.date.toString()}"
+            timeView.text = "Time: ${data.time.toString()}"
+            contactInfoView.text = "Contact at ${data.contact.toString()}"
+            generalInfoView.text = data.description.toString()
             Log.d("SinglePost", data.title.toString())
         }
     }
