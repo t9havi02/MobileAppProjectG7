a4b9b9a93e1d9c14345e4ccb0d16c03cf039d335
Artem Sitnov <stnoov@gmail.com>
2021-04-23 12:53:16 +0300

    implement creation of premium posts

--- app/src/main/java/fi/oamk/groupfinderapp/CreatePostActivity.kt
+++ app/src/main/java/fi/oamk/groupfinderapp/CreatePostActivity.kt
@@ -1,13 +1,17 @@
 package fi.oamk.groupfinderapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.view.WindowManager
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_create_post.*
 
 class CreatePostActivity: AppCompatActivity() {
     private lateinit var database: DatabaseReference
@@ -20,6 +24,8 @@ class CreatePostActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_post)
+
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
         edTitle = findViewById(R.id.title)
         edDate = findViewById(R.id.date)
@@ -37,10 +43,25 @@ class CreatePostActivity: AppCompatActivity() {
         val contact = edContact.text.toString()
         val description = edDescription.text.toString()
 
-
         val key = database.child( "posts").push().key.toString()
         val item = Item(key, title, date, time, contact, description)
-        database.child( "posts").child(key).setValue(item)
+        if(check_premium.isChecked) {
+            database.child( "pposts").child(key).setValue(item)
+                    .addOnSuccessListener {
+                        Toast.makeText(baseContext, "Premium post was created",
+                                Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+        } else {
+            database.child( "posts").child(key).setValue(item)
+                    .addOnSuccessListener {
+                        Toast.makeText(baseContext, "Post created",
+                                Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+        }
     }
 
 
